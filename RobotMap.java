5edbc6ba374656c25c55e5af6eb6b7aecf88693b
Eric Schultz <eric@griznmoose.org>
2016-10-11 16:10:38 -0700

    Create RobotMap.java

---
+++ RobotMap.java
@@ -0,0 +1,25 @@
+// DEFINE WHAT PACKAGE THIS IS IN
+// IMPORT CORRECT PACKAGES
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+public class RobotMap {
+	public static CANTalon frontLeft;
+	public static CANTalon backLeft;
+	public static CANTalon frontRight;
+	public static CANTalon backRight;
+    
+	public static RobotDrive driveRobot;
+    
+    public static Solenoid solenoid;
+    public static Compressor compressor;
+	
+	public static void init() {
+        frontRight = new CANTalon(1);
+        backRight = new CANTalon(2);
+        backLeft = new CANTalon(3);
+		frontLeft = new CANTalon(4);
+		driveRobot = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+	}
+}
