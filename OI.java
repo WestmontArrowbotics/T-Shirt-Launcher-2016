f05017b8ee1e91d163d3eafd915178fa1df35a1a
nvan04 <nhihvan@gmail.com>
2016-10-11 16:17:10 -0700

    Create OI.java

---
+++ OI.java
@@ -0,0 +1,15 @@
+public class OI {
+
+	public Joystick xboxController;
+
+	public OI() {
+		xboxController = new Joystick(0);
+	}
+  
+	public Joystick getxboxController() {
+		return xboxController;
+	}
+  
+  //Awaiting button...
+	
+}
